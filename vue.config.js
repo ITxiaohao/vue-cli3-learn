8da351fc1195af8360f8e5a9d5691f382da3511a
Itxiaohao <shunhaozeng@gmail.com>
2019-01-16 10:25:00 +0800

    :wrench: 更新 webpack4 拆分代码配置

--- vue.config.js
+++ vue.config.js
@@ -44,32 +44,30 @@ module.exports = {
     config.module.rule('images').test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
 
     // 使用 webpack4 新特性来拆分代码
-    const splitOptions = config.optimization.get('splitChunks')
-    config.optimization.splitChunks(
-      Object.assign({}, splitOptions, {
-        chunks: 'all',
-        cacheGroups: {
-          libs: {
-            name: 'chunk-libs',
-            test: /[\\/]node_modules[\\/]/,
-            priority: 10,
-            chunks: 'initial' // 只打包初始时依赖的第三方
-          },
-          elementUI: {
-            name: 'chunk-elementUI', // 单独将 elementUI 拆包
-            priority: 20, // 权重要大于 libs 和 app 不然会被打包进 libs 或者 app
-            test: /[\\/]node_modules[\\/]element-ui[\\/]/
-          },
-          commons: {
-            name: 'chunk-comomns',
-            test: resolve('src/components'), // 可自定义拓展你的规则
-            minChunks: 2, // 最小共用次数
-            priority: 5,
-            reuseExistingChunk: true
-          }
+    config.optimization.splitChunks({
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: 'initial' // 只打包初始时依赖的第三方
+        },
+        elementUI: {
+          name: 'chunk-elementUI', // 单独将 elementUI 拆包
+          priority: 20, // 权重要大于 libs 和 app 不然会被打包进 libs 或者 app
+          test: /[\\/]node_modules[\\/]element-ui[\\/]/
+        },
+        commons: {
+          name: 'chunk-commons',
+          test: resolve('src/components'), // 可自定义拓展你的规则
+          minChunks: 3, // 最小公用次数
+          priority: 5,
+          reuseExistingChunk: true
         }
-      })
-    )
+      }
+    })
+    config.optimization.runtimeChunk('single')
   },
 
   configureWebpack: {
